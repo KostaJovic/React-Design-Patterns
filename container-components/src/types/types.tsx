aa79805b3facf9cb1c39ee877a057689e83dcd89
KostaJovic <kosta.j@icloud.com>
2025-11-07 08:30:45 +0000

    clean up default files and install express and cors

---
+++ container-components/src/types/types.tsx
@@ -0,0 +1,15 @@
+export type Person = {
+    id: string;
+    name: string;
+    age: number;
+    hairColor: string;
+    hobbies: string[];
+}
+
+export type Product = {
+    id: string;
+    name: string;
+    price: string;
+    description: string;
+    rating: number;
+}
